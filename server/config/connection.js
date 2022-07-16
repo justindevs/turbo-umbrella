be846ca7b9ca827ec8c9007cc145df394f74801d
Justin Gottschalk <justin@Justins-MacBook-Pro.local>
2022-07-16 12:01:04 -0500

    scaffolding 3

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoString = process.env.MONGODB_URI ? `${process.env.MONGODB_URI}/googlebooks` : 'mongodb://localhost/googlebooks';
+const mongoString = process.env.MONGODB_URI ? `${process.env.MONGODB_URI}` : 'mongodb://localhost/googlebooks';
 
 console.log(mongoString)
 
